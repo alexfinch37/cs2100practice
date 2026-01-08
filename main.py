e651abe1a35b20136910a443755302dfd9f52834
alexfinch <alex@finch.industries>
2026-01-08 11:32:20 -0500

    end thurs

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 """
 TODO: A very useful temperature-conversion app.
 """
-print("hello world!")
+def main() -> None:
+    print("hello world!")
+
+    your_name: str = input("What is your name? ")
+    print(f'howdy {your_name}')
+
+if __name__ == "__main__":
+    main()
+    
